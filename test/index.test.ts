c013d8f616919a44454446f05b332033405f31e8
Christian de Wit <christian@cleverfranke.com>
2023-04-20 10:45:29 +0200

    Added test for timing

--- test/index.test.ts
+++ test/index.test.ts
@@ -40,7 +40,7 @@ test('Unsubscribe return value', async () => {
   expect(success).toBe(true);
 });
 
-test('Unsubscribe return value second time', async () => {
+test('Unsubscribe return value second time', () => {
   const unsubscribe = onFrame(() => {
     // noop
   });
@@ -49,4 +49,20 @@ test('Unsubscribe return value second time', async () => {
   const success = unsubscribe();
 
   expect(success).toBe(false);
+});
+
+test('Timing', () => {
+  vi.useFakeTimers();
+
+  const start = Date.now();
+
+  const unsubscribe = onFrame(() => {
+    const now = Date.now();
+    expect(now - start).toBe(16);
+  });
+
+  vi.advanceTimersByTime(16);
+
+  unsubscribe();
+  vi.useRealTimers();
 });
